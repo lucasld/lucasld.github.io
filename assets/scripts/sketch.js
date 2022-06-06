fe3992d4ebddb4666e99d448e5456b8474ba3a9a
lucasld <lucas.ld@hermes-link.de>
2022-06-06 10:49:52 +0200

    added own logo

--- assets/scripts/sketch.js
+++ assets/scripts/sketch.js
@@ -1,11 +1,63 @@
+let particle_color;
+let particleSystem;
+
+
 function setup() {
-    const canvas = createCanvas(windowWidth * 0.8, 100)
-    canvas.parent('sketch-holder')
+    const canvas = createCanvas(35, 35);
+    canvas.parent("sketch1");
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        particle_color = color(255);
+    } else {
+        particle_color = color(0);
+    }
+    particleSystem = new ParticleSystem(30);
 }
-  
   
 function draw() {
-    background(0);
-    fill(255, 0, 0);
-    ellipse(mouseX, mouseY, 100, 100)
+    clear();
+    particleSystem.step();
 }
+
+
+class Particle {
+    constructor() {
+        this.x = width/2;
+        this.y = height/2;
+        this.s = random(80, 160);
+        this.r = random(7, 10);
+    }
+
+    move() {
+        this.x = width/2 + cos(this.s + frameCount/this.s) * this.r + sin(this.s + frameCount/this.s/2) * this.r/2;
+        this.y = height/2 + sin(this.s + frameCount/this.s) * this.r;
+    }
+}
+
+class ParticleSystem {
+    constructor(particle_number) {
+        this.particles = Array(particle_number);
+        for (var i=0; i<particle_number; i++){
+            this.particles[i] = new Particle();
+        }
+    }
+
+    step() {
+        // move all particles
+        this.particles.forEach(x => x.move());
+        // draw particles
+        for (var i=0; i<this.particles.length; i++){
+            let particle = this.particles[i];
+            stroke(particle_color);
+            strokeWeight(2);
+            //fill (255, 0, 0);
+            //ellipse(particle.x, particle.y, 30, 30);
+            point (particle.x, particle.y);
+        }
+    }
+}
+
+// change background color if switch in theme is detected
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    particle_color = event.matches ? color(255) : color(0);
+});
